707b44359b1d11a673c2b90b5536500e9789cfb0
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-04-23 17:42:46 +0100

    calculate the age

--- lib/calcAge.dart
+++ lib/calcAge.dart
@@ -1,4 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+
 class Age {
+  
+  static int days;
+  static int months;
+  static int years;
+  // int currentDay;
+  // int currentMonth;
+  // int currentYear;
+
+  // int currentDay = int.parse(DateFormat("dd").format(DateTime.now()));
+  // int currentMonth = int.parse(DateFormat("MM").format(DateTime.now()));
+  // int currentYear = int.parse(DateFormat("yyyy").format(DateTime.now()));
+
+  // get the exact days in the year
+  _exactDay(int month, int year) {
+    int daysEx;
+    if (month == 1 ||
+        month == 3 ||
+        month == 5 ||
+        month == 7 ||
+        month == 8 ||
+        month == 10 ||
+        month == 12) {
+      daysEx = 31;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+      daysEx = 30;
+    } else {
+      if (year % 4 == 0) {
+        daysEx = 29;
+      } else {
+        daysEx = 28;
+      }
+    }
+    return daysEx;
+  }
+
+  static getYears(int birthYear, int currentYear) {
+    years = currentYear - birthYear;
+    return years;
+  }
+
+  static getMonths(int birthMonth, int currentMonth) {
+    if (currentMonth - birthMonth >= 0) {
+      months = currentMonth - birthMonth;
+    } else {
+      months = ((currentMonth + 12) - birthMonth);
+      Age.years -= 1;
+    }
+    return months;
+  }
+
+  static getDays(int birthDay, int currentDay) {
+    if (currentDay - birthDay >= 0) {
+      days = (currentDay - birthDay);
+    } else {
+      days = ((currentDay - birthDay) + birthDay);
+      Age.months -= 1;
+    }
+    return days;
+  }
+
+  static getNextYears() {}
+
+  static getNextMonths(int currMonth, int birthMonth) {
+    int nextMonth = 0;
+    if (birthMonth - currMonth >= 0) {
+      nextMonth = birthMonth - currMonth;
+    } else {
+      nextMonth = (birthMonth + 12 - currMonth) - 1;
+    }
+    return nextMonth;
+  }
+
+  static getNextDays(int currDays, int birthDay) {
+    int nextDays;
+    if (birthDay - currDays >= 0) {
+      nextDays = (birthDay - currDays);
+    } else {
+      nextDays = ((birthDay + 31) - currDays) - 1;
+    }
+    return nextDays;
+  }
+
   static ageCalculator(int year) {
     int currentYear = DateTime.now().year;
     int resutl = currentYear - year;
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'calcAge.dart';
 
 main() => runApp(MyApp());
 
@@ -22,12 +22,53 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var prColor = Color(0xFF23B27D);
+  int nextYear = 0;
+  int nextMonths = 0;
+  int nextDays = 0;
+
+  int yourYears = 0;
+  int yourMonths = 0;
+  int yourDays = 0;
+  int dayPicked = 0;
+  int monthPicked = 0;
+  int yearPicked = 0;
 
   @override
   Widget build(BuildContext context) {
+    int currentDay, currentMonth, currentYear;
     Size screen_size = MediaQuery.of(context).size;
     var textStyle1 =
         TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: prColor);
+
+    Future showDatePicker1() async {
+      DateTime date = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
+          lastDate: DateTime(2100));
+      setState(() {
+        dayPicked = int.parse(DateFormat("dd").format(date));
+        monthPicked = int.parse(DateFormat("MM").format(date));
+        yearPicked = int.parse(DateFormat("yyyy").format(date));
+      });
+    }
+
+    currentDay = int.parse(DateFormat("dd").format(DateTime.now()));
+    currentMonth = int.parse(DateFormat("MM").format(DateTime.now()));
+    currentYear = int.parse(DateFormat("yyyy").format(DateTime.now()));
+
+    void calculateAge() {
+      setState(() {
+        yourYears = Age.getYears(yearPicked, currentYear);
+        yourMonths = Age.getMonths(monthPicked, currentMonth);
+        yourDays = Age.getDays(dayPicked, currentDay);
+        nextMonths = Age.getNextMonths(currentMonth, monthPicked);
+        nextDays = Age.getNextDays(currentDay, dayPicked);
+        print(Age.months);
+        print(Age.years);
+      });
+    }
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
@@ -40,42 +81,6 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             Text(
-              "Today's Date",
-              textAlign: TextAlign.center,
-              style: textStyle1,
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Container(
-              width: screen_size.width,
-              height: 60,
-              decoration:
-                  BoxDecoration(border: Border.all(color: prColor, width: 2)),
-              child: Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  SizedBox(
-                    width: 40,
-                  ),
-                  Text(
-                    "30" + "  /  " + "05" + "  /  " + "1997",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  FlatButton(
-                    child: Image(image: AssetImage("images/calendar.png")),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
               "Date of Birth",
               textAlign: TextAlign.center,
               style: textStyle1,
@@ -86,25 +91,98 @@ class _HomeScreenState extends State<HomeScreen> {
             Container(
               width: screen_size.width,
               height: 60,
+              padding: EdgeInsets.fromLTRB(35, 0, 0, 0),
               decoration:
                   BoxDecoration(border: Border.all(color: prColor, width: 2)),
               child: Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SizedBox(
-                    width: 40,
-                  ),
+                  // SizedBox(
+                  //   width: 40,
+                  // ),
+                  Text("$dayPicked",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
                   Text(
-                    "30" + "  /  " + "05" + "  /  " + "1997",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                    "/",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                   ),
+                  Text("$monthPicked",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
+                  Text(
+                    "/",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  Text("$yearPicked",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
                   SizedBox(
-                    width: 20,
+                    width: 10,
                   ),
-                  FlatButton(
-                    child: Image(image: AssetImage("images/calendar.png")),
-                    onPressed: () {},
-                  )
+                  Container(
+                    width: screen_size.width * 0.2,
+                    height: 60,
+                    color: prColor,
+                    child: FlatButton(
+                      child: Image(image: AssetImage("images/calendar.png")),
+                      onPressed: () {
+                        showDatePicker1();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Today's Date",
+              textAlign: TextAlign.center,
+              style: textStyle1,
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(35, 0, 0, 0),
+              width: screen_size.width,
+              height: 60,
+              decoration:
+                  BoxDecoration(border: Border.all(color: prColor, width: 2)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("$currentDay",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
+                  Text(
+                    "/",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  Text("$currentMonth",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
+                  Text(
+                    "/",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  Text("$currentYear",
+                      style:
+                          TextStyle(fontSize: 27, fontWeight: FontWeight.w700)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: screen_size.width * 0.2,
+                    height: 60,
+                    color: prColor,
+                    child: FlatButton(
+                      child: Image(image: AssetImage("images/calendar.png")),
+                      onPressed: () {},
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -119,15 +197,21 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     RaisedButton(
+                      splashColor: Colors.white,
                       child: Text(
                         "CALCULATE",
                         style: TextStyle(fontSize: 23),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          calculateAge();
+                        });
+                      },
                       textColor: Colors.white,
                       color: prColor,
                     ),
                     RaisedButton(
+                      splashColor: Colors.white,
                       child: Text(
                         "CLEAR",
                         style: TextStyle(fontSize: 23),
@@ -147,7 +231,8 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Card(
               elevation: 2,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
@@ -157,31 +242,43 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Text("Years",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor)),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor)),
                         Text("Months",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor)),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor)),
                         Text("Days",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor))
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor))
                       ],
                     ),
                     SizedBox(
                       height: 16,
                     ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                      Text("23",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700])),
-                      Text("10",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700])),
-                      Text("22",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700]))
-                    ]),
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("$yourYears",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700])),
+                          Text("$yourMonths",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700])),
+                          Text("$yourDays",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700]))
+                        ]),
                   ],
                 ),
               ),
@@ -189,13 +286,15 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 8,
             ),
-            Text("Next Birthday", textAlign: TextAlign.center, style: textStyle1),
+            Text("Next Birthday",
+                textAlign: TextAlign.center, style: textStyle1),
             SizedBox(
               height: 4,
             ),
             Card(
               elevation: 2,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
@@ -205,31 +304,43 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Text("Years",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor)),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor)),
                         Text("Months",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor)),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor)),
                         Text("Days",
                             style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500,color: prColor))
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: prColor))
                       ],
                     ),
                     SizedBox(
                       height: 16,
                     ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                      Text("23",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700])),
-                      Text("10",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700])),
-                      Text("22",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500,color: Colors.grey[700]))
-                    ]),
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("$nextYear",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700])),
+                          Text("$nextMonths",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700])),
+                          Text("$nextDays",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey[700]))
+                        ]),
                   ],
                 ),
               ),
